5330759a304c41ab10898657135f33f70e3ddacb
tanphat0226 <phatpham0226@gmail.com>
2025-04-23 12:36:36 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1>HEllo</h1>
+      <h1>HE2llo</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SideBar = () => {
+  return (
+    <aside>
+      <h2>SideBar</h2>
+    </aside>
+  )
+}
+
+export default SideBar
